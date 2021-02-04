bb44fb979d74bdc79574010b575e2abafd78be0f
Zach Andrews <ztandrews18@sbcglobal.net>
2021-02-03 23:12:17 -0500

    Updates
    A few small changes to make it look a little better

--- LouCoinDictionary.py
+++ LouCoinDictionary.py
@@ -3,6 +3,8 @@
 Created on Fri Jan 29 19:18:06 2021
 
 @author: ztand
+The offical back end to the LouCoin tracker
+Code is neatly commented to make it easy to follow along on what is going on
 """
 
 from sportsipy.nhl.roster import Player, Roster
@@ -25,14 +27,13 @@ def calculateLouCoin(Player):
 
 
 #List of players that are either not skaters or have never played
-neverPlayed = ['Cory Schneider','Jakub Skarek','Semyon Varlamov','Ilya Sorokin', 'Thomas Hickey','Dmytro Timashov', 'Tom Kuhnhackl', 'Otto Koivula', 'Austin Czarnik', 'Sebastian Aho']
+neverPlayed = ['Cory Schneider','Jakub Skarek','Semyon Varlamov','Ilya Sorokin', 'Thomas Hickey', 'Tom Kuhnhackl', 'Otto Koivula', 'Sebastian Aho']
 
 #Variable for total LouCoin mined so far. Will get calculated in next loop
 totalLouCoin = 0
 
 #Loop through the players, calculate their Lou Coin, add to total LouCoin, add to dict
 for i in iroster:
-    #printPlayer(i)
     if i.name in neverPlayed:
         continue
     else:
@@ -40,10 +41,11 @@ for i in iroster:
         totalLouCoin = playersLouCoin+totalLouCoin
         islesDict[i.name] = playersLouCoin
 
-#Sort the dictionary that holds islanders players and their lou coin values
+#Sort the dictionary that holds Islanders players and their LouCoin values
 def sortByValue():
     sortedByValueDict = sorted(islesDict.items(), key=lambda t:t[1], reverse=True)
-    #print(sortedByValueDict)
+    #sortedByValueDict is actually an array that is sorted. Each element is an element of the islesDict 
+    #This loops through the array and cleans it up by getting rid of unneeded characters
     for player in sortedByValueDict:
         p0 = str(player)
         p1 = p0.replace('(','')
@@ -51,7 +53,9 @@ def sortByValue():
         p3 = p2.replace("'",'')
         print(p3)
 
+#Calls the sort function and prints out the result. Will then print out the total amount of LouCoin mined
 sortByValue()
 print()
 print("Total LouCoin Mined:")
 print(totalLouCoin)
+#Now, take the output and copy it into the list to html converter website guy
